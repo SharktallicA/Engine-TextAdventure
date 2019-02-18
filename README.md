be4b8cf53b80712589844145fe2786a334e04bc3
SharktallicA <22660310+SharktallicA@users.noreply.github.com>
2019-02-18 11:15:05 +0000

    Populate README and stage basic project

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Engine-TextAdventure
+Prototype C++ text adventure engine that runs a game from a text file without the need for recompilation. It is being developed for GNU/Linux with CLion, C++14, and minimum CMake 3.13.
---
+++ cmake-build-debug/CMakeFiles/Engine_TextAdventure.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Engine_TextAdventure.dir/main.cpp.o"
+  "Engine_TextAdventure.pdb"
+  "Engine_TextAdventure"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Engine_TextAdventure.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
