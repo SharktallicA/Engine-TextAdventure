b1e0ac1831f9603c2ec15e7af0f5b24e847e1ffd
SharktallicA <22660310+SharktallicA@users.noreply.github.com>
2019-02-22 21:17:57 +0000

    Prototype map assembler added (not tested)

--- Engine/GameLoader.h
+++ Engine/GameLoader.h
@@ -39,36 +39,57 @@ private:
     {
         try
         {
-            // 1) Construct basic place objects from traversing all XML Area notes,
-            // using only the area's name for constructor purposes at this point
+            // 1) Construct all individual place objects via Area XML
+            // names without mapping for the moment. Just store adjacent
+            // neighbour data in two arrays since all data needs to be
+            // read before pointer links can be established
 
+            //All individual places
             vector<Place*> places;
+            //Four possible neighbour names for each place
+            vector<string[4]> adjacentNames;
+            //Four possible neighbour travel dsitances for each place
+            vector<int[4]> adjacentCosts;
+
             for (xml_node area = doc.child("Game").child("Area"); area; area = area.next_sibling("Area"))
+            {
                 places.push_back(new Place(area.attribute("name").as_string()));
 
-            // 2) Maps out the relative (north, east, south, west) adjacent places
-            // for each Place object; for each Place object in places vector, use the raw
-            // place data to find out the names of its neighbours and them link
-            // their place objects into the current one (and repeat) until (an up to
-            // four-way) doubly-linked list is formed
+                string adjName[4];
+                adjName[0] = area.child("NorthName").child_value();
+                adjName[1] = area.child("EastName").child_value();
+                adjName[2] = area.child("SouthName").child_value();
+                adjName[3]  = area.child("WestName").child_value();
+                adjacentNames.push_back(adjName);
 
-            //NOT WORKING: ported from Game-TextAdventure
+                int adjDist[4];
+                adjDist[0] = (int)area.child("NorthDistance").child_value()[0];
+                adjDist[1] = (int)area.child("EastDistance").child_value()[0];
+                adjDist[2] = (int)area.child("SouthDistance").child_value()[0];
+                adjDist[3]  = (int)area.child("WestDistance").child_value()[0];
+                adjacentCosts.push_back(adjDist);
+            }
+
+            // 2) For each individual place, find the neighbour objects
+            // based on their name and create the (up to four-way) doubly
+            // linked list from it. If the place has no neighbour for a direction,
+            // nullptr is used to indicate lack of elsewhere in the program.
+
             for (int i = 0; i < places.size(); i++)
             {
-                vector<Place*> relativePlaces;
+                vector<PlaceData*> relatives;
 
-                //find and get the Place object adjacent to the current one
-                for (int j = 1; j <= 4; j++)
+                for (int j = 0; j < 4; j++)
                 {
-                    if (raw[i][j] != "-") //work out the relative Place if one is specified in the raw data
+                    if (adjacentNames[i][j] != "")
                     {
-                        for (Place* place : places) if (place->GetName() == raw[i][j]) relativePlaces.push_back(place);
+                        for (Place* place : places) if (place->GetName() == adjacentNames[i][j])
+                                relatives.push_back(new PlaceData(place, adjacentCosts[i][j]));
                     }
-                    else relativePlaces.push_back(nullptr); //nullptr to represent blank wall
+                    else relatives.push_back(new PlaceData(nullptr, 0));
                 }
 
-                //call for the mapping of the places to form the linked bonds
-                places[i]->MapPlaces(relativePlaces[0], relativePlaces[1], relativePlaces[2], relativePlaces[3]);
+                places[i]->MapPlaces(relatives[0], relatives[1], relatives[2], relatives[3]);
             }
         }
         catch(...) { throw "FATAL ERROR: cannot load <Map> from game file!"; }
